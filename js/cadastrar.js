a28413ba262120dd7c2ade553f35c8507a88ba63
freitazgiovanna <giovanna.bezerra@sptech.school>
2024-04-25 21:25:25 -0300

    BootCamp 25/04 - Aula final

---
+++ js/cadastrar.js
@@ -0,0 +1,32 @@
+async function cadastrarAgenda() {
+    const nomeDigitado = document.getElementById("nomeJogo").value;
+    const dataDigitada = document.getElementById("dataJogo").value;
+    const descricaoDigitada = document.getElementById("descricao").value;
+    const gamersDigitados = document.getElementById("gamers").value;
+    const urlImagemDigitada = document.getElementById("urlImagem").value;
+    const urlLiveDigitada = document.getElementById("urlLive").value;
+
+
+    const dadosAgenda = {
+        nome: nomeDigitado,
+        dataJogo: dataDigitada,
+        descricao: descricaoDigitada,
+        gamers: gamersDigitados.split(","),
+        urlImagem: urlImagemDigitada,
+        urlLive: urlLiveDigitada
+    }
+
+    const respostaCadastro = await fetch("https://660f44b1356b87a55c510dd2.mockapi.io/agendas", {
+        method: "POST",
+        body: JSON.stringify(dadosAgenda),
+        headers: {
+            "Content-type": "application/json ; charset=UTF-8"
+        }
+    });
+
+    if (respostaCadastro.status === 201) {
+        window.location.href = "agenda.html";
+    } else {
+        alert("Erro ao cadastrar agenda");
+    }
+}
